11257a58a19151e7a6d3a09980f788db09e47236
VladisLove82 <zloe.moloco@gmail.com>
2024-11-27 14:52:24 +0300

    module_4_2.py
    Исправил возврат return и добавил коммент

--- module_4_2/module_4_2.py
+++ module_4_2/module_4_2.py
@@ -1,7 +1,7 @@
 def test_function():
     def inner_function():
         print("Я в области видимости функции test_function")
-        return  inner_function()
+    return test_function()
 
-inner_function()
-test_function()
+
+inner_function() # Ошибка возникает из за того, что наша функция inner_function() не определена
